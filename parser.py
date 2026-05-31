8f073dabc1ace68c096373a2bbe9f7689283b002
Laisczt <laissopaes@gmail.com>
2026-05-31 14:03:27 -0300

    fix: mensagem de erro do parser

--- parser.py
+++ parser.py
@@ -13,17 +13,17 @@ def parse_file(filepath) -> list[Point]:
             line = line.strip()
             split = re.split(r', |,| ', line)
             if len(split) != 2:
-                raise ValueError(f'Formatação do arquivo de entrada incorreto:\n {i}:{line}')
+                raise ValueError(f'Formatação do arquivo de entrada incorreto:\n{i}:{line}')
 
             try:
                 x = float(split[0])
             except ValueError as e:
-                raise ValueError(f'Valor de X não pode ser convertido em float:\n {i}:{line}\n{'^' * len(split[0])}\n{e}')
+                raise ValueError(f'Valor de X não pode ser convertido em float:\n{i}:{line}\n{' ' * (len(str(i)) + 1)}{'^' * len(split[0])}\n{e}')
 
             try:
                 y = float(split[1])
             except ValueError as e:
-                raise ValueError(f'Valor de Y não pode ser convertido em float:\n {i}:{line}\n{' ' * (len(line) - len(split[1]))}{'^' * len(split[1])}')
+                raise ValueError(f'Valor de Y não pode ser convertido em float:\n{i}:{line}\n{' ' * (len(str(i)) + 1)}{' ' * (len(line) - len(split[1]))}{'^' * len(split[1])}\n{e}')
 
             graph.append(point.Point(x, y))
             
